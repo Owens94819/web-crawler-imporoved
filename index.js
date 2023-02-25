3fee9573eeeb05630d507dfaaf6a45cad6ff44af
owens <65927654+Owens94819@users.noreply.github.com>
2023-02-25 15:48:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
-const parseURL = function (url) {
-    url = url || "";
-    var loc = {}
-    url = url.trim().replace(/\n/img, '').toLowerCase().replace(/\\/img, '/')
-    loc.href = url.trim().replace(/\n/img, '').toLowerCase()
-    loc.origin = [loc.href.match(/^https?\:[\/\\][\/\\]\w[^\/\\#]+\w/)].toString() || "null"
 
-    if (loc.origin !== "null") {
-        loc.__path__ = loc.href.replace(loc.origin, '')
-        loc.parentpathname = loc.__path__.replace(/#[^]+|\?[^]+/, '').trim() || '/'
-        loc.protocol = [loc.origin.match(/^https?\:/)].toString()
-        loc.host = loc.origin.replace(/^https?\:[\\\/][\\\/]/, '')
-        loc.hostname = loc.host.replace(/\:[^]*/, '')
-        loc.__hash__ = [loc.__path__.match(/#[^?]+/, '')].toString().substring(1)
-        loc.hash = [loc.__path__.match(/#[^]+/, '')].toString()
-        loc.qeury = [loc.__path__.match(/\?[^#]+/, '')].toString()
-        var pathparse = loc.parentpathname.split('/')
-        loc.parentpathname = []
-        for (var i = 0; i < pathparse.length; i++) {
-            if (pathparse[i] === "..") {
-                loc.parentpathname.pop()
-                continue;
-            } else if (pathparse[i] === "." || (pathparse[i] === "" && i + 1 < pathparse.length)) {
-                continue;
-            }
-            loc.parentpathname.push(pathparse[i])
-        }
 
-        loc.pathname = '/' + loc.parentpathname.join('/')
-        loc.parentpathname.pop()
-        loc.parentpathname = '/' + loc.parentpathname.join('/')
-        loc.href = loc.origin + loc.pathname + loc.hash;
-    } else {
-        loc.protocol = [loc.href.match(/^[^\\\/\#\?]+\:/)].toString()
-        loc.pathname = loc.href.replace(/^[^\\\/\#\?]+\:/, '')
-    }
-    return loc
-}
-parseURL.join = function (par, chd, loc) {
-    if (arguments.length === 1) {
-        chd = par
-    }
-    chd = chd.trim().replace(/\\/img, '/')
-    if (parseURL.isdomain(chd)) {
-        return parseURL(chd)
-    }
-    if (!(loc instanceof Object)) {
-        if (typeof globals === 'object') {
-            loc = globals.location
-        } else {
-            loc = parseURL(par)
-        }
-    }
-    if (chd[0] === "/") {
-        chd = chd.substring(1)
-        loc = loc.origin
-    } else {
-        loc = loc.origin + loc.parentpathname
-    }
-    loc = parseURL(loc + '/' + chd)
-    return loc
-}
 
-parseURL.isdomain = function (url) {
-    return url.search(/^https?:\/\/+[^]*[:.]+\w|^[^\\\/\#\?]+\:/) >= 0
-}
+
+const zlib = require('zlib');
+
+const gzip = zlib. createGzip();
+
+// const fs = require('fs');
+
+// const inp = fs. createReadStream('test. txt');
+
+// const out = fs. createWriteStream('test. txt. gz');
 
 
 
 const express = require('express'),
 compression = require('compression'),
+parseURL = require ('./parseURL.js');
 protocol={
 "http:" : require('http'),
 "https:" :require('https'),
@@ -131,12 +79,15 @@ if(!http) return res.status(404), res.send("invalid url");
  http =    http.get(url,
         function (req){
           // console.log(req.headers);
+  // application/x-gzip
 
              res.status(req.statusCode);
-             res.setHeader('content-type', req.headers['content-type'])
-         
-           // res.write("12345");
-            req.pipe(res);
+             res.setHeader('content-type', "application/x-gzip"||req.headers['content-type'])
+      
+    // req. pipe(gzip). pipe(res);
+           
+// res.write("12345");
+          //  req.pipe(res);
         }
     )
 
