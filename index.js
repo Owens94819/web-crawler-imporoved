c46d585f27dbf423179ad965c61c41ad360a29b3
owens <65927654+Owens94819@users.noreply.github.com>
2023-04-09 19:57:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -131,11 +131,12 @@ app.use("/anti-cors*", function (req, res, next) {
     //console.log(req.path)
 
     url = "https://url.url/" + decodeURIComponent(req.originalUrl.replace(/\/?anti\-cors\/?/, ""));
-url= new URL(url). href.replace("https://url.url/","");
+url= new URL(url)
+url=url . href.replace("https://url.url/","");
 
 console.log(url)
 
-    url = new URL(url));
+    url = new URL(url)
     
 
 console.log(url)
